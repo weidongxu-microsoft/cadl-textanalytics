f4c989ff10ddfae7da376f15139dafffdcfb3b48
Haoling Dong <haolingdong@microsoft.com>
2022-09-07 11:18:23 +0800

    regen

--- azure-ai-language-authoring/src/main/java/com/azure/ai/language/authoring/models/SwapDeploymentsOptions.java
+++ azure-ai-language-authoring/src/main/java/com/azure/ai/language/authoring/models/SwapDeploymentsOptions.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 /**
  * Swap deployment options
  *
- * <p>define first name and second name for swap deployment options.
+ * <p>Define first name and second name for swap deployment options.
  */
 @Immutable
 public final class SwapDeploymentsOptions {
